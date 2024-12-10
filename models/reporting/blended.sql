5342850fd7ce340e8a6591d0eae402dee2a3ff8e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-12-10 22:03:25 +0100

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -10,9 +10,10 @@ union all
 select date, date_granularity, sum(spend) as spend from reporting.bite_tiktok_ad_performance group by 1,2)
 group by 1,2)
 
-, sho_data as (select date, date_granularity, orders, subtotal_sales as revenue from reporting.bite_shopify_sales)
+, sho_data as (select date, date_granularity, orders, subtotal_sales as revenue, first_orders, first_order_subtotal_sales as first_orders_revenue 
+    from reporting.bite_shopify_sales)
 
 select
 date, date_granularity,
-coalesce(spend,0) as spend, orders, revenue
+coalesce(spend,0) as spend, orders, revenue, first_orders, first_orders_revenue
 from sho_data left join spend_data using(date, date_granularity)
