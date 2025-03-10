a1cac5980e8216b06114564ed93d0480a79f8232
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-10 15:04:50 -0600

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -2,18 +2,24 @@
     alias = target.database + '_blended'
 )}}
 
-with spend_data as (select channel, date, date_granularity, sum(spend) as spend from 
-(select 'Meta' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_facebook_ad_performance group by 1,2,3
-union all
-select 'Google Ads' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_googleads_campaign_performance group by 1,2,3
-union all
-select 'TikTok' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_tiktok_ad_performance group by 1,2,3)
-group by 1,2,3)
+with spend_data as 
+    (select channel, date, date_granularity, coalesce(sum(spend),0) as spend 
+    from 
+        (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_facebook_ad_performance group by 1,2,3
+        union all
+        select 'Google Ads' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_googleads_campaign_performance group by 1,2,3
+        union all
+        select 'TikTok' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_tiktok_ad_performance group by 1,2,3)
+        group by 1,2,3),
 
-, sho_data as (select 'Shopify' as channel, date, date_granularity, orders, subtotal_sales as revenue, first_orders, first_order_subtotal_sales as first_orders_revenue 
-    from reporting.bite_shopify_sales)
+sho_data as 
+    (select 'Shopify' as channel, date, date_granularity, 
+        coalesce(sum(orders),0) as orders, coalesce(sum(subtotal_sales),0) as revenue, coalesce(sum(first_orders),0) as first_orders, coalesce(sum(first_order_subtotal_sales),0) as first_orders_revenue 
+    from reporting.bite_shopify_sales
+    group by 1,2,3)
 
 select
-channel, date, date_granularity,
-coalesce(spend,0) as spend, orders, revenue, first_orders, first_orders_revenue
-from sho_data left join spend_data using(date, date_granularity, channel)
+    channel, date, date_granularity,
+    spend, orders, revenue, first_orders, first_orders_revenue
+from sho_data 
+left join spend_data using(date, date_granularity, channel)
