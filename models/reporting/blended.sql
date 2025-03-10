fbfece1b9d0163e5fd3091bc83b4ccb8ccc46b4a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-10 14:53:35 -0600

    Update blended.sql
    channel

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -2,18 +2,18 @@
     alias = target.database + '_blended'
 )}}
 
-with spend_data as (select date, date_granularity, sum(spend) as spend from 
-(select date, date_granularity, sum(spend) as spend from reporting.bite_facebook_ad_performance group by 1,2
+with spend_data as (select channel, date, date_granularity, sum(spend) as spend from 
+(select 'Meta' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_facebook_ad_performance group by 1,2,3
 union all
-select date, date_granularity, sum(spend) as spend from reporting.bite_googleads_campaign_performance group by 1,2
+select 'Google Ads' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_googleads_campaign_performance group by 1,2,3
 union all
-select date, date_granularity, sum(spend) as spend from reporting.bite_tiktok_ad_performance group by 1,2)
-group by 1,2)
+select 'TikTok' as channel, date, date_granularity, sum(spend) as spend from reporting.bite_tiktok_ad_performance group by 1,2,3)
+group by 1,2,3)
 
-, sho_data as (select date, date_granularity, orders, subtotal_sales as revenue, first_orders, first_order_subtotal_sales as first_orders_revenue 
+, sho_data as (select 'Shopify' as channel, date, date_granularity, orders, subtotal_sales as revenue, first_orders, first_order_subtotal_sales as first_orders_revenue 
     from reporting.bite_shopify_sales)
 
 select
-date, date_granularity,
+channel, date, date_granularity,
 coalesce(spend,0) as spend, orders, revenue, first_orders, first_orders_revenue
-from sho_data left join spend_data using(date, date_granularity)
+from sho_data left join spend_data using(date, date_granularity, channel)
