cdf26ee08c9c183c3535166ec9376bb82c9dccb0
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-11 07:53:41 -0600

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -5,17 +5,17 @@
 with spend_data as 
     (select channel, date, date_granularity, coalesce(sum(spend),0) as spend 
     from 
-        (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_facebook_ad_performance group by 1,2,3
+        (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'facebook_ad_performance') }} group by 1,2,3
         union all
-        select 'Google Ads' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_googleads_campaign_performance group by 1,2,3
+        select 'Google Ads' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'googleads_campaign_performance') }} group by 1,2,3
         union all
-        select 'TikTok' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from reporting.bite_tiktok_ad_performance group by 1,2,3)
+        select 'TikTok' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'tiktok_ad_performance') }} group by 1,2,3)
         group by 1,2,3),
 
 sho_data as 
     (select 'Shopify' as channel, date, date_granularity, 
         coalesce(sum(orders),0) as orders, coalesce(sum(subtotal_sales),0) as revenue, coalesce(sum(first_orders),0) as first_orders, coalesce(sum(first_order_subtotal_sales),0) as first_orders_revenue 
-    from reporting.bite_shopify_sales
+    from {{ source('reporting', 'bite_shopify_sales') }}
     group by 1,2,3)
 
 select
