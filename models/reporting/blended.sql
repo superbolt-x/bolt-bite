fbac661a3b5af11255df8ecb57fadd1232e76393
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-11 09:05:50 -0600

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -4,7 +4,7 @@
 
 with spend_data as 
     (select channel, date, date_granularity, 
-        coalesce(sum(spend),0) as spend, coalesce(sum(paid_orders),0) as paid_orders 
+        coalesce(sum(spend),0) as spend, coalesce(sum(paid_orders),0) as paid_orders,
         0 as orders, 0 as revenue, 0 as first_orders, 0 as first_orders_revenue
     from 
         (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend, coalesce(sum(purchases),0) as paid_orders from {{ source('reporting', 'facebook_ad_performance') }} group by 1,2,3
@@ -15,7 +15,7 @@ with spend_data as
         group by 1,2,3),
 
 sho_data as 
-    (select 'Shopify' as channel, date, date_granularity, 0 as spend, 0 as paid_purchases,
+    (select 'Shopify' as channel, date, date_granularity, 0 as spend, 0 as paid_orders,
         coalesce(sum(orders),0) as orders, coalesce(sum(subtotal_sales),0) as revenue, coalesce(sum(first_orders),0) as first_orders, coalesce(sum(first_order_subtotal_sales),0) as first_orders_revenue 
     from {{ source('reporting', 'shopify_sales') }}
     group by 1,2,3)
