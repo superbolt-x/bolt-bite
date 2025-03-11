f4d4d532b540d743a35133e11161b9497c8541fc
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-11 07:59:17 -0600

    Update blended.sql
    paid_orders

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -3,23 +3,28 @@
 )}}
 
 with spend_data as 
-    (select channel, date, date_granularity, coalesce(sum(spend),0) as spend 
+    (select channel, date, date_granularity, 
+        coalesce(sum(spend),0) as spend, coalesce(sum(paid_orders),0) as paid_orders 
+        0 as orders, 0 as revenue, 0 as first_orders, 0 as first_orders_revenue
     from 
-        (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'facebook_ad_performance') }} group by 1,2,3
+        (select 'Meta' as channel, date, date_granularity, coalesce(sum(spend),0) as spend, coalesce(sum(purchases),0) as paid_orders from {{ source('reporting', 'facebook_ad_performance') }} group by 1,2,3
         union all
-        select 'Google Ads' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'googleads_campaign_performance') }} group by 1,2,3
+        select 'Google Ads' as channel, date, date_granularity, coalesce(sum(spend),0) as spend, coalesce(sum(purchases),0) as paid_orders from {{ source('reporting', 'googleads_campaign_performance') }} group by 1,2,3
         union all
-        select 'TikTok' as channel, date, date_granularity, coalesce(sum(spend),0) as spend from {{ source('reporting', 'tiktok_ad_performance') }} group by 1,2,3)
+        select 'TikTok' as channel, date, date_granularity, coalesce(sum(spend),0) as spend, coalesce(sum(purchases),0) as paid_orders from {{ source('reporting', 'tiktok_ad_performance') }} group by 1,2,3)
         group by 1,2,3),
 
 sho_data as 
-    (select 'Shopify' as channel, date, date_granularity, 
+    (select 'Shopify' as channel, date, date_granularity, 0 as spend, 0 as paid_purchases,
         coalesce(sum(orders),0) as orders, coalesce(sum(subtotal_sales),0) as revenue, coalesce(sum(first_orders),0) as first_orders, coalesce(sum(first_order_subtotal_sales),0) as first_orders_revenue 
     from {{ source('reporting', 'bite_shopify_sales') }}
     group by 1,2,3)
 
 select
     channel, date, date_granularity,
-    spend, orders, revenue, first_orders, first_orders_revenue
-from sho_data 
-left join spend_data using(date, date_granularity, channel)
+    spend, paid_orders, orders, revenue, first_orders, first_orders_revenue
+from 
+    (select * from spend_data
+    union all
+    select * from paid_data)
+    
